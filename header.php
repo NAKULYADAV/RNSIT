7501f7d2349aa50e8162a48cd01241b04bb674df
NAKUL <nakul.yadava@gmail.com>
2015-01-15 09:57:04 +0530

    hearder.

---
+++ header.php
@@ -0,0 +1,9 @@
+<html>
+<body bgcolor=666666>         
+<table width=100% bgcolor=0066cc >
+<td>
+<center>
+<img src="images/logo.jpg" height=118px alt="R.N.S. INSTIOTUTE OF TECNOLOGY"><img src="images/header.jpg" alt="R.N.S. INSTIOTUTE OF TECNOLOGY">
+</center>
+</td>
+</table>
